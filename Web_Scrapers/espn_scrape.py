7d2c1f6a51c44c5544517a7064fbde89b1df43ad
unknown <ming_ying@college.harvard.edu>
2018-07-25 23:54:05 -0400

    Commented ESPN scrape file

--- Web_Scrapers/espn_scrape.py
+++ Web_Scrapers/espn_scrape.py
@@ -1,4 +1,6 @@
-## Scrapes deposits by state data from FDIC website over selected period of years.
+## Scrapes ESPN player data by game for every active player in the MLB, and exports
+## each player's data as a CSV file.
+##
 ## To run this file, pip install the packages below and install the chromedriver
 ## application onto your computer and create a PATH variable to the chromedriver
 ## folder.
@@ -13,48 +15,57 @@ from bs4 import BeautifulSoup as BS
 import csv, time, os
 
 def main():
+	# Start scraping from here (any player's page will do)
 	url = "http://www.espn.com/mlb/player/_/id/32355/nick-ahmed"
+
+	# Write CSV files to this folder: change this
 	out = "C:/Users/Ming/Documents/Fantasy_Models/MLB_data"
+
 	select(url = url, out = out)
 
 def select(url, out):
+	# Initialize Chromedriver
 	driver = webdriver.Chrome()
+
+	# Go to page
 	driver.get(url)
 
-	# page loading wait object
+	# Open player dropdown menu
 	wait = WebDriverWait(driver, 100)
-
 	wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "split-select"))).click()
 
+	# Wait for menu to be clickable
 	teams_menu = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "main-items")))
 
 	wait = WebDriverWait(teams_menu, 100)
 
+	# Get IDs of all teams
 	wait.until(EC.element_to_be_clickable((By.XPATH, "./li/a[@href = '#']")))
 	teams_list = teams_menu.find_elements_by_xpath("./li/a[@href = '#']")
 	team_ids = map(lambda x: x.get_attribute("id"), teams_list)
 
 	for i in range(0, len(team_ids)):
 		wait = WebDriverWait(driver, 100)
+
+		# Make sure that dropdown menu is open
 		if(i > 0):
 			wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "split-select"))).click()
 	
-		# hover over the chosen team
+		# Hover over the chosen team
 		wait = WebDriverWait(driver, 100)
 		wait.until(EC.element_to_be_clickable((By.ID, team_ids[i]))).click()
 
-		# open menu of all teams
+		# Open players menu
 		wait.until(EC.presence_of_element_located((By.CLASS_NAME, "split-level-content-list")))
-
-		# get players menu element
 		players_menu = driver.find_elements_by_class_name("split-level-content-list")
 
-		# list of all players on a specific team
+		# Get list of all players on a specific team
 		wait = WebDriverWait(players_menu[i], 100)
 
 		wait.until(EC.element_to_be_clickable((By.XPATH, "./li")))
 		team_players = players_menu[i].find_elements_by_xpath("./li/a")
 
+		# Get list of links to all player's bio pages
 		links = []
 		for x in team_players:
 			link = x.get_attribute("href").split("/")
@@ -63,23 +74,26 @@ def select(url, out):
 
 		for j in range(0, len(links)):
 			try:
-				# go to individual player's game logs
+				# Go to individual player's game logs
 				driver.get(links[j])
 
 				# HTML of game logs
 				game_page = driver.page_source
 				soup = BS(game_page, "lxml")
 
+				# Scrape the game log table
 				data = soup.findAll('table', class_="tablehead")[0]
 				header = data.find_all("tr", class_="colhead")[0]
 				subheaders = data.find_all("tr", class_=["bi"])
 				data_by_row = data.find_all("tr", class_=["evenrow","oddrow"])
 				data_by_row = [header] + [x for x in data_by_row if x not in subheaders]
 				
+				# Convert table elements to text
 				rows = []
 				for row in data_by_row:
 					rows.append([val.text.encode('utf8') for val in row.find_all("td")])
 
+				# Write to a CSV file
 				player_name = links[j].split("/")[-1]
 
 				file_name = "{}.csv".format(player_name)
