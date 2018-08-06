8b792729da7f9fdfb9e034b0633be621dc8c2424
unknown <ming_ying@college.harvard.edu>
2018-08-06 12:58:28 -0400

    Cleaned up commentation

--- MLB_Models/baseball1.R
+++ MLB_Models/baseball1.R
@@ -20,6 +20,8 @@ library(doParallel)
 ## Allow parallel processing
 
 ## ------------------------------------------------------------ ##
+
+
 num.cores = detectCores()
 cl <- makePSOCKcluster(num.cores)
 registerDoParallel(cl)
@@ -28,6 +30,7 @@ registerDoParallel(cl)
 ## Cleans Rotogrinders CSV files
 
 ## ------------------------------------------------------------ ##
+
 
 clean.rotogrinders = function(roto.path) {
   df = read.csv(roto.path,
@@ -713,16 +716,26 @@ backtest = function(overlaps, salary.cap,
 }
 
 
+# Overlaps to test
 overlaps = 3:7
+
+# Salary cap
 salary.cap = 50000
+
+# Lineups to generate per entry
 num.lineups = 150
+
+# Dates to test
 year = 2018
 month = 6
 days = list("4" = 30, "5" = 31, "6" = 30, "7" = 31, "8" = 4)
+
+# Paths to required folders
 path.hitters.proj = "C:/Users/Ming/Documents/Fantasy_Models/Historical_Projections_MLB/Hitters/hitter_YEAR-0MONTH-DAY.csv"
 path.pitchers.proj = "C:/Users/Ming/Documents/Fantasy_Models/Historical_Projections_MLB/Pitchers/pitcher_YEAR-0MONTH-DAY.csv"
 path.players.actual = "C:/Users/Ming/Documents/Fantasy_Models/Actual_Scores_MLB/players_YEAR-0MONTH-DAY.csv"
 
+# Run backtesting (this will take hours and hours)
 backtest(overlaps, salary.cap,
          num.lineups, path.hitters.proj,
          path.pitchers.proj, path.players.actual,
